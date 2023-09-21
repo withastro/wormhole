6d86275a597c7d1727e4a8f64c00be95be719985
Nate Moore <nate@astro.build>
2023-09-20 20:16:02 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Wormhole
+
+Wormhole is [Vercel Edge Function](https://vercel.com/docs/functions/edge-functions/quickstart) that acts as a shortcut to the [Astro Docs](https://docs.astro.build/en/getting-started/).
+
+## Usage
+
+Just pass your query as the path, and you will be redirected to the most relevant page of the Astro Docs.
+
+- `https://example.com/vercel` => `https://docs.astro.build/[lang]/guides/deploy/vercel/`
+- `https://example.com/islands` => `https://docs.astro.build/[lang]/concepts/islands/`
+
+## Thanks
+
+- [`@lazd`](https://blog.lazd.net/) for [`mdn.io`](https://github.com/lazd/mdn.io), which was the inspiration for this project.
+- [DuckDuckGo](https://duckduckgo.com/) for powering the search!
---
+++ api/index.ts
@@ -0,0 +1,35 @@
+export const config = {
+    runtime: 'edge',
+}
+
+const engine = new URL('https://duckduckgo.com/');
+const domain = new URL('https://docs.astro.build');
+const redirect = (url: string | URL) => new Response(null, { status: 301, headers: { Location: url.toString() }})
+
+export default async function handler(request: Request) {
+    // TODO: Accept-Language support?
+
+    // We use [duckduckgo bangs](https://duckduckgo.com/bangs)
+    // to get the most relevant search result for our domain.
+    const searchURL = new URL(engine);
+    const query = new URL(request.url).searchParams.get('q');
+    if (!query) {
+        return redirect(domain);
+    }
+    searchURL.searchParams.set('q', `! site:${domain} ${query}`);
+    try {
+        const html = await fetch(searchURL, { method: 'GET' }).then(res => res.text());
+        // DuckDuckGo rewrites the request to a client-side redirect for tracking.
+        // This pulls the final URL out of the markup.
+        // 
+        // WARNING: extremely likely to break in the future!
+        // It will fallback to the original request if it does.
+        const [_, ddgPath] = /content='0;\s*url=([^']+)'/gi.exec(html) ?? []
+        if (!ddgPath) throw new Error();
+        const finalURL = new URL(ddgPath, searchURL).searchParams.get('uddg');
+        if (!finalURL) throw new Error();
+        return redirect(finalURL)
+    } catch {}
+
+    return redirect(searchURL);
+}
